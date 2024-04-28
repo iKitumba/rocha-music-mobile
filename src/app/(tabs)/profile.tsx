c3d0089e21d01eea3d3297b40213ede2c593ea3d
Alberto Kitumba <alberkitumba@gmail.com>
2024-04-28 20:47:47 +0100

    Configure tabs router structure

---
+++ src/app/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const ProfilePage = () => {
+  return (
+    <View className="flex-1 bg-slate-950 items-center justify-center">
+      <Text className="text-2xl text-slate-50">ProfilePage</Text>
+    </View>
+  );
+};
+
+export default ProfilePage;
---
+++ src/app/(tabs)/publish.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const PublishPage = () => {
+  return (
+    <View className="flex-1 bg-slate-950 items-center justify-center">
+      <Text className="text-2xl text-slate-50">PublishPage</Text>
+    </View>
+  );
+};
+
+export default PublishPage;
