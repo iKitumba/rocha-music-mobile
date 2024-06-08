2ec739977b76543604dac5ee4720044d6c6275e3
Alberto Kitumba <alberkitumba@gmail.com>
2024-06-08 19:53:44 +0100

    feat: Add profile header

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,10 +1,21 @@
+import { ChevronLeft, Ellipsis } from "lucide-react-native";
 import React from "react";
-import { View, Text } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
 const ProfilePage = () => {
   return (
-    <View className="flex-1 bg-slate-950 items-center justify-center">
-      <Text className="text-2xl text-slate-50">ProfilePage</Text>
+    <View className="flex-1 bg-slate-950">
+      <View className="pt-16 px-6 pb-2 flex-row items-center justify-between">
+        <TouchableOpacity className="w-10 h-10 bg-slate-800 items-center justify-center rounded-full">
+          <ChevronLeft color={"white"} size={24} />
+        </TouchableOpacity>
+
+        <Text className="text-slate-50 font-medium font-base">_iamafonso</Text>
+
+        <TouchableOpacity className="w-10 h-10 bg-slate-800 items-center justify-center rounded-full">
+          <Ellipsis color={"white"} size={24} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
